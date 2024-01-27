3698083c31f9f840999c55571725ebaa582af5f2
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-01-27 20:08:42 +0530

    fix / fix

--- pages/api/ShowCoffeeItemClient.js
+++ pages/api/ShowCoffeeItemClient.js
@@ -5,9 +5,11 @@ export default async function ShowCoffeeItemClient(req, res) {
   if (req.method == "GET") {
     try {
       DbConnection();
-      let data = await CoffeeItemSchema.find({Active:"ON"}).select('-createdAt -updatedAt');
+      let data = await CoffeeItemSchema.find({ Active: "ON" }).select(
+        "-createdAt -updatedAt"
+      );
 
-      res.status(201).json({ status: "201",data });
+      res.status(201).json({ status: "201", data });
     } catch (error) {
       res.status(501).json({ message: error, status: "501" });
     }
