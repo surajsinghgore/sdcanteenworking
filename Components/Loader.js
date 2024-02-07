752fcc6b5980fc1c40a2f7c0aa57cb8fe1b0126c
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-07 17:48:23 +0530

    style/ loader responsive

--- Components/Loader.js
+++ Components/Loader.js
@@ -1,17 +1,27 @@
 import Image from "next/image";
 let LoaderImage = `https://res.cloudinary.com/dnxv21hr0/image/upload/v1681014247/loader_a4xalk.gif`;
 
-export default function Loader({loader}) {
+export default function Loader({ loader }) {
   return (
     <>
-    {(loader)? <div className="b">
-    <div className="bg">
-    <div className="loader">
-    <Image src={LoaderImage} layout='fill'
-    objectFit='contain' alt="loader" className="image" priority="true"/>
+      {loader ? (
+        <div className="loader_Container">
+         
+            <div className="loader">
+              <Image
+                src={LoaderImage}
+                layout="fill"
+                objectFit="contain"
+                alt="loader"
+                className="image"
+                priority="true"
+              />
+            </div>
+         
         </div>
-        </div>
-        </div> : ""}
+      ) : (
+        ""
+      )}
     </>
-  )
+  );
 }
