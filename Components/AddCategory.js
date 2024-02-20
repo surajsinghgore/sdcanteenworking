0f2ed44c4c8f82d2171756907fce6d884be1e47c
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-02-20 20:22:48 +0530

    fixing

--- Components/AddCategory.js
+++ Components/AddCategory.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import StyleFood from "../styles/AddFood.module.css";
 import HeadTag from "../Components/Head";
 import AdminLeftMenu from "../Components/AdminLeftMenu";
